0d3658789662b974d09161672f04ea771ed75922
Mohammad Wasil <mwasil@outlook.co.id>
2023-04-26 11:55:42 +0200

    enable blog and fix edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,11 +41,12 @@ const config = {
                     sidebarPath: require.resolve('./sidebars.js'),
                     editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
                 },
-                blog: false,
-                // blog: {
-                //     showReadingTime: true,
-                //     editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
-                // },
+                blog: {
+                     showReadingTime: true,
+                     editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
+                     editUrl: ({locale, blogDirPath, blogPath, permalink}) =>
+                        `https://github.com/${organizationName}/${projectName}/edit/main/${blogDirPath}/${blogPath}`,
+                },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
                 },
